bca72714172fe59c9aa0626b36d866f96e2de26c
Aness Chelfat <106586310+0xpix@users.noreply.github.com>
2026-09-12 08:49:43 +0200

    test(brouter): compile upstream map creator fixture support

--- router-brouter/build.gradle.kts
+++ router-brouter/build.gradle.kts
@@ -18,11 +18,16 @@ sourceSets {
         )
         resources.srcDir("../vendor/brouter/misc/profiles2")
     }
+    named("test") {
+        java.srcDir("../vendor/brouter/brouter-map-creator/src/main/java")
+    }
 }
 
 dependencies {
     api(project(":router-api"))
     implementation(project(":router-core"))
     implementation(libs.coroutines.core)
+
     testImplementation(libs.junit)
+    testImplementation("org.openstreetmap.osmosis:osmosis-osm-binary:0.48.3")
 }
